51674f6e3ddf2f67fef8832e853ea76002016c42
Telnesito <carlosj_1703@hotmail.com>
2023-12-25 19:32:11 -0400

    Ya se muestra la info de los cursos de la base de datos

--- Bugzzy/src/pages/ReproductorMP4.jsx
+++ Bugzzy/src/pages/ReproductorMP4.jsx
@@ -7,31 +7,61 @@ import VideoPlayer from "../components/VideoPlayer";
 import video from "../assets/videos/video.mp4";
 import { SearchNavbar } from "../components/SearchNavbar";
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { obtenerCursoPorId } from '../services/cursos';
+import { login } from '../services/usuarios';
 const ReproductorMP4 = ({ title, description, labelText, secondLabelText, thirdLabelText, images, comments }) => {
-    const url = useParams()
-    console.log(url)
+    const { id } = useParams()
+    const [curso, setCurso] = useState({})
+
+    useEffect(() => {
+
+        const cargarData = async () => {
+            const response = await obtenerCursoPorId(id)
+            const creador = await login(response.cedulaCreador)
+
+            setCurso({ creador, response })
+        }
+
+        cargarData()
+
+
+    }, [id])
+
+    console.log(curso)
+
+
     return (
-        <div style={{ paddingBottom: '50px' }}> {/* Aquí se agrega el relleno */}
-            <SearchNavbar /> {/* Aquí se pasa la prop showSearchBar */}
-            <div style={{ backgroundColor: 'black' }}>
-                <div className="flex justify-center items-center mt-16">
-                    <VideoPlayer src={video} />
-                </div>
-            </div>
-            <div style={{ marginLeft: '15%', fontSize: '32px', fontWeight: '700', letterSpacing: '-0.16px', textTransform: 'uppercase', color: '#141E34', lineHeight: '40px', paddingTop: '20px', paddingRight: '5px' }}>
-                <div style={{ display: 'flex' }}>
-                    <p style={{ paddingRight: '5px' }}>{labelText} |</p>
-                    <p style={{ color: 'var(--Go-to-actions-secondary, #15665A)', fontSize: '24px', fontStyle: 'normal', fontWeight: '700', lineHeight: '40px', letterSpacing: '-0.16px', textTransform: 'uppercase', paddingTop: '2px' }}>{secondLabelText}</p>
-                </div>
-            </div>
-            <div style={{ marginLeft: '15%', fontSize: '32px', fontWeight: '700', letterSpacing: '-0.16px', textTransform: 'uppercase', color: '#141E34', lineHeight: '40px' }}>
-                <p style={{ color: 'var(--input-icon-color, #979CB6)', fontSize: '20px', fontStyle: 'normal', fontWeight: '700', lineHeight: '40px', letterSpacing: '-0.16px', textTransform: 'uppercase' }}>{thirdLabelText}</p>
-            </div>
-            <SimpleAccordion title={title} description={description} />
-            <ComplexAccordion images={images} /> {/* Aquí se usa el componente ComplexAccordion */}
-            <CommentAccordion comments={comments} /> {/* Aquí se usa el componente CommentAccordion */}
-            <Footer />
-        </div>
+        <>
+            {
+                curso.response && (
+                    <div style={{ paddingBottom: '50px' }}> {/* Aquí se agrega el relleno */}
+                        <SearchNavbar /> {/* Aquí se pasa la prop showSearchBar */}
+                        <div style={{ backgroundColor: 'black' }}>
+                            <div className="flex justify-center items-center mt-16">
+                                <VideoPlayer src={curso.response.video_file} />
+                            </div>
+                        </div>
+                        <div style={{ marginLeft: '15%', fontSize: '32px', fontWeight: '700', letterSpacing: '-0.16px', textTransform: 'uppercase', color: '#141E34', lineHeight: '40px', paddingTop: '20px', paddingRight: '5px' }}>
+                            <div style={{ display: 'flex' }}>
+                                <p style={{ paddingRight: '5px' }}>{curso.response.tituloPublicacion} |</p>
+                                <p style={{ color: 'var(--Go-to-actions-secondary, #15665A)', fontSize: '24px', fontStyle: 'normal', fontWeight: '700', lineHeight: '40px', letterSpacing: '-0.16px', textTransform: 'uppercase', paddingTop: '2px' }}>{secondLabelText}</p>
+                            </div>
+                        </div>
+                        <div style={{ marginLeft: '15%', fontSize: '32px', fontWeight: '700', letterSpacing: '-0.16px', textTransform: 'uppercase', color: '#141E34', lineHeight: '40px' }}>
+                            <p style={{ color: 'var(--input-icon-color, #979CB6)', fontSize: '20px', fontStyle: 'normal', fontWeight: '700', lineHeight: '40px', letterSpacing: '-0.16px', textTransform: 'uppercase' }}>{curso.creador.usuario}</p>
+                        </div>
+                        <SimpleAccordion title={'Descripcion'} description={curso.response.descripcionPublicacion} />
+                        <ComplexAccordion images={images} /> {/* Aquí se usa el componente ComplexAccordion */}
+                        <CommentAccordion comments={comments} /> {/* Aquí se usa el componente CommentAccordion */}
+                        <Footer />
+                    </div>
+                )
+
+            }
+        </>
+
+
     );
 }
 
